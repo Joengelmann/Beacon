d9cfdf47375752a8674e01fa02955a3b17da8a3e
Jonah Engelmann <Joengelmann@scu.edu>
2024-02-17 11:18:29 -0800

    Initial Commit

---
+++ Beacon/BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconApp.swift
+//  Beacon
+//
+//  Created by Jonah Engelmann on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
