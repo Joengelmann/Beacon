b7354b3d80291369363cdc207542d0376c5623d7
Jonah Engelmann <Joengelmann@scu.edu>
2024-02-17 11:35:55 -0800

    First commit

--- Beacon/BeaconApp.swift
+++ Beacon/BeaconApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct BeaconApp: App {
+struct YourAppNameApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
--- Beacon/ContentView.swift
+++ Beacon/ContentView.swift
@@ -6,19 +6,49 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+            MainView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            NetworkView()
+                .tabItem {
+                    Image(systemName: "network")
+                    Text("Network")
+                }
         }
-        .padding()
+    }
+}
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+struct MainView: View {
+    var body: some View {
+        Button(action: {
+            print("Button pressed!")
+        }) {
+            Text("Press Me")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(100)
+                .frame(width: 200, height: 200)
+        }
+    }
+}
+struct NetworkView: View {
+    var body: some View {
+        Text("Network")
     }
 }
 
-#Preview {
-    ContentView()
-}
